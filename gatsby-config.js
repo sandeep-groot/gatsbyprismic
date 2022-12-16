a50c9f0f4858363a2ead9da8ccc593b8ba0224ae
sandeep <sandeep.gill@grootsoftwares.com>
2022-12-16 15:29:21 +0530

    creating prismic with gatsby project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,52 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+// const routes = [
+//   // {
+//   //   type: "article",
+//   //   path: "/articles/:uid",
+//   // },
+//   // {
+//   //   type: "page",
+//   //   path: "/:uid",
+//   // },
+// ];
+
 module.exports = {
   siteMetadata: {
     title: `gatsbyprismic`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        // schemas: {
+        //   post: require("./src/schemas/articles.json"),
+        // },
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        // routes,
+      },
+    },
+  ],
 };
---
+++ src/components/image.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "test-image.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  if (!data?.placeholderImage?.childImageSharp?.fluid)
+    return <div>Picture not found</div>;
+
+  return <Img fluid={data?.placeholderImage?.childImageSharp?.fluid} />;
+};
+
+export default Image;
