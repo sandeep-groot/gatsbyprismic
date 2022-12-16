bbd1ee0634d576e66309ca0ca79dc142cc02c930
sandeep <sandeep.gill@grootsoftwares.com>
2022-12-16 18:43:48 +0530

    add prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,15 +34,15 @@ module.exports = {
       },
       __key: "images",
     },
-    // {
-    //   resolve: "gatsby-source-prismic",
-    //   options: {
-    //     repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
-    //     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-    //     customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
-    //     linkResolver: require("./linkResolver").linkResolver,
-    //     // routes,
-    //   },
-    // },
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        linkResolver: require("./linkResolver").linkResolver,
+        // routes,
+      },
+    },
   ],
 };
